464355db9ed27115b6a79a2387d2134d36c632c2
Jesstin <jjodhanto@gmail.com>
2022-11-20 22:01:01 +0700

    first commit

---
+++ sisfoGAUPH/app/Http/Controllers/Admin/reservationController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+use App\Models\reservation;
+use Illuminate\Http\Request;
+
+class reservationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->get('search');
+        $perPage = 25;
+
+        if (!empty($keyword)) {
+            $reservation = reservation::where('name', 'LIKE', "%$keyword%")
+                ->latest()->paginate($perPage);
+        } else {
+            $reservation = reservation::latest()->paginate($perPage);
+        }
+
+        return view('admin.reservation.index', compact('reservation'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('admin.reservation.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+        
+        $requestData = $request->all();
+        
+        reservation::create($requestData);
+
+        return redirect('admin/reservation')->with('flash_message', 'reservation added!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $reservation = reservation::findOrFail($id);
+
+        return view('admin.reservation.show', compact('reservation'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $reservation = reservation::findOrFail($id);
+
+        return view('admin.reservation.edit', compact('reservation'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $requestData = $request->all();
+        
+        $reservation = reservation::findOrFail($id);
+        $reservation->update($requestData);
+
+        return redirect('admin/reservation')->with('flash_message', 'reservation updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        reservation::destroy($id);
+
+        return redirect('admin/reservation')->with('flash_message', 'reservation deleted!');
+    }
+}
