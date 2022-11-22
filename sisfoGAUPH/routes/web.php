a7e30338249764827ffe3c17986ba21c96a729af
Jesstin <jjodhanto@gmail.com>
2022-11-22 21:55:38 +0700

    Kuis

--- sisfoGAUPH/routes/web.php
+++ sisfoGAUPH/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\laporanController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::resource('admin/category', 'App\\Http\\Controllers\\Admin\categoryController');
 Route::resource('admin/user-role', 'App\\Http\\Controllers\\Admin\userRoleController');
 Route::resource('admin/reservation-status', 'App\\Http\\Controllers\\Admin\reservationStatusController');
@@ -24,3 +26,6 @@ Route::resource('admin/missing-item-status', 'App\\Http\\Controllers\\Admin\miss
 Route::resource('admin/attendance', 'App\\Http\\Controllers\\Admin\attendanceController');
 Route::resource('admin/reservation', 'App\\Http\\Controllers\\Admin\reservationController');
 Route::resource('admin/schedule', 'App\\Http\\Controllers\\Admin\scheduleController');
+Route::resource('admin/pengajuan-replacement-class', 'App\Http\Controllers\Admin\pengajuanReplacementClassController');
+Route::resource('admin/laporan/replacement', 'App\Http\Controllers\laporanController');
+Route::get('admin/laporan/replacement/{id}/pdf', [laporanController::class, 'pdf']);
